2c1c4adf83364055f6bd0dd98fcc366a5d080038
Sanjeev <sanjeev2838@gmail.com>
2014-04-07 17:10:54 +0530

    initializatione rror

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,6 +1,11 @@
-Spree.config do |config|
-  config = Rails.application.config
-  config.spree.calculators.tax_rates << AusShippingCalculator
-  config.spree.calculators.shipping_methods << AusShippingCalculator
-  config.spree.calculators.promotion_actions_create_adjustments << AusShippingCalculator
-end
+ config = Rails.application.config
+ # config.spree.calculators.tax_rates << AusShippingCalculator
+ # config.spree.calculators.shipping_methods << AusShippingCalculator
+ # config.spree.calculators.promotion_actions_create_adjustments << AusShippingCalculator
+
+ #initializer 'spree.register.calculators' do |app|
+ #  app.config.spree.calculators.shipping_methods << Spree::Calculator::AusShippingCalculator
+ #end
+ #config.after_initialize do
+ #  config.spree.calculators.tax_rates << Spree::Calculator::Shipping::AusShippingCalculator
+ #end
